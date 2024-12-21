9dc1d58a114744a556f65fc745e7fe6fabc08e09
Mohibbur Rahman <mohibbur1994@gmail.com>
2024-12-21 13:03:07 +0600

    update 12/25

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,7 +23,7 @@
 <body class="font-sans antialiased">
     {{-- <x-banner /> --}}
 
-    <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
+    <div class="min-h-screen bg-white dark:bg-gray-800">
         {{-- @livewire('navigation-menu') --}}
         @include('layouts.header')
         @include('layouts.sidebar')
